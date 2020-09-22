02f98f6b6a7f603f511ec99d5b2b4b67fb260983
Abel Lamesgen <abeltyr@gmail.com>
2020-09-22 15:05:37 +0300

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,7 @@ import Head from 'next/head'
 const House = props => (
   <div>
     <Head>
-      <title>The page title do 2</title>
+      <title>The page title do 2 00 2</title>
     </Head>
     <p style={{ fontFamily: "'Lora', serif", fontStyle: "italic", fontWeight: 900, fontSize: 30 }}> Looking into this</p>
     {/* the rest of the JSX */}
